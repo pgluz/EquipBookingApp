7450f8480682dce58bf0ba4f96268d3865dfce11
pgluz <kontakt@gluz.pl>
2026-05-24 12:55:34 +0200

    Dodano modele do DB + pierwsza migracja

---
+++ EquipBooking.Api/Models/Reservation.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace EquipmentBooking.Api.Models;
+
+public class Reservation
+{
+public int Id { get; set; }
+public string Purpose { get; set; } = string.Empty;
+public DateTime StartDate { get; set; }
+public DateTime EndDate { get; set; }
+public string Status { get; set; } = "Oczekująca"; // Oczekująca, Zaakceptowana, Odrzucona
+
+// Relacje
+public int EquipmentId { get; set; }
+public Equipment? Equipment { get; set; }
+
+public int UserId { get; set; }
+public User? User { get; set; }
+}
